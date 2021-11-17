8768f63429974e07ff4c4ba3440f6e136e2c9dc2
Zac5566 <l5125@yahoo.com.tw>
2021-11-17 21:09:13 +0800

    2-5 增加logback,修改啟動時提示

--- src/main/java/com/example/wiki/WikiApplication.java
+++ src/main/java/com/example/wiki/WikiApplication.java
@@ -1,13 +1,20 @@
 package com.example.wiki;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@Slf4j
 public class WikiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        ConfigurableApplicationContext app = SpringApplication.run(WikiApplication.class, args);
+        Environment env = app.getEnvironment();
+        log.info("啟動成功");
+        log.info("地址: \thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
