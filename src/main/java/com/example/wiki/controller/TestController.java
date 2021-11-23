8c9c394101e8d48582e4361a148e3f0f31f40381
Zac5566 <l5125@yahoo.com.tw>
2021-11-23 19:57:49 +0800

    2-7 使用http client測試

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.example.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("hello")
+    @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String hello(String name){
+        return "hello post" + name;
     }
 }
