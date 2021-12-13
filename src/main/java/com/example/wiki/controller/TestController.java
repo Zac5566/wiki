8facb8192f88af69d08785963e0778df6dd1a752
Zac5566 <l5125@yahoo.com.tw>
2021-12-13 21:52:12 +0800

    2-8 使用自訂義的properties，加冒號設定預設值

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World"+testHello;
     }
 
     @PostMapping("/hello/post")
